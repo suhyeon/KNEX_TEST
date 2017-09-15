6a2e30cc536b4d4fd262c07d17930533818a71c5
Suhyeon JO <flora12375@gmail.com>
2017-09-15 16:00:24 +0900

    knex bcrypt password

--- wpsn-knex/migrations/20170915154646_convert_password_to_bcrypt.js
+++ wpsn-knex/migrations/20170915154646_convert_password_to_bcrypt.js
@@ -3,10 +3,13 @@ const bcrypt = require('bcrypt')
 exports.up = function(knex, Promise) {
   return knex('user')
   .then(users => {
-    const hash = bcrypt.hashSync(user.password,10)
-    knex('user')
-    .where({id: user.id})
-    .update({password: hash})
+    const promises = users.map(user => { //promise로 된 모든 것이 전부 성공한 뒤에 하고 싶은 의도이라면 all을 사용
+      const hash = bcrypt.hashSync(user.password,10) //users.map의 반환은 promise의 배열이다.
+      return knex('user')
+      .where({id: user.id})
+      .update({password: hash})
+    })
+    return Promise.all(promises)
   })
 };
 
